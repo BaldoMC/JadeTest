e82f001f80ef154dafecc7799bfe6a9ff0bda303
Baldo Morales Choque <32346079+BaldoMC@users.noreply.github.com>
2018-01-10 11:55:04 -0300

    Creado metodo de mensaje
    Metodo que manda un mensaje a "Joaquin"

--- src/PrimerAgente.java
+++ src/PrimerAgente.java
@@ -1,9 +1,18 @@
 import jade.core.Agent;
+import jade.lang.acl.ACLMessage;
+import jade.core.AID;
 
 public class PrimerAgente extends Agent {
 	
 	protected void setup() {
 		System.out.println("Hola!, mi nombre es: " + getAID().getName());
+		
+		ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+		msg.addReceiver(new AID("Joaquin", AID.ISLOCALNAME));
+		msg.setContent("Hola!");
+		msg.setLanguage("Espaniol");
+		send(msg);
 	}
-
+	
+	
 }
