bb0c5f63b4962d90c970aa4b6bc7339b92dec4b6
Baldo Morales Choque <32346079+BaldoMC@users.noreply.github.com>
2018-01-10 12:54:42 -0300

    Documentación agregada
    Cambios en los mensajes, ahora se muestra quien envio el mensaje.

--- src/PrimerAgente.java
+++ src/PrimerAgente.java
@@ -11,18 +11,29 @@ public class PrimerAgente extends Agent {
 	protected void setup() {
 		System.out.println("Hola!, mi nombre es: " + getAID().getName());
 		
+		//Se crea un mensaje con PERFORMATIVE = INFORM
 		ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+		//Se agrega el receptor a Joaquin
 		msg.addReceiver(new AID("Joaquin", AID.ISLOCALNAME));
+		//Se agrega el contenido del mensaje
 		msg.setContent("Hola");
+		//Se define el lenguaje del mensaje
 		msg.setLanguage("Espaniol");
+		//Se envia el mensaje
 		send(msg);
 		
+	    /**
+	     * Metodo que agrega un comportamiento ciclico
+	     * El cual recibe un mensaje y dependiendo del texto, responde.
+	     */
 		addBehaviour(new CyclicBehaviour() {
 		
 			@Override
 			public void action() {
+				//Se obtiene el mensaje de la pila de mensajes
 				ACLMessage mensaje = receive();
 				if(mensaje!= null) {
+					//Se crea una respuesta a ese mensaje
 					ACLMessage respuesta = mensaje.createReply();
 					if(mensaje.getContent().contains("como estas")) {
 						
@@ -35,13 +46,16 @@ public class PrimerAgente extends Agent {
 						
 						}
 					}
-					JOptionPane.showMessageDialog(null,"Mensaje: "+ mensaje.getContent());
+					//Se muestra por un mensaje el contenido
+					JOptionPane.showMessageDialog(null,"Mensaje de: " +mensaje.getSender().getLocalName() +" con texto: " + mensaje.getContent());
+					//Se pausa por 3 segundos
 					try{ 
 						Thread.sleep(3000); 
 					} catch(InterruptedException e ){ 
 						System.out.println("Thread Interrupted");
 						
 					}
+					//Se envia la respuesta
 					myAgent.send(respuesta);
 				}else
 					block();
@@ -49,4 +63,6 @@ public class PrimerAgente extends Agent {
 			}
 		});
 	}
+	
+	
 }
--- src/SegundoAgente.java
+++ src/SegundoAgente.java
@@ -9,14 +9,20 @@ public class SegundoAgente extends Agent{
 	protected void setup() {
 		System.out.println("Hey!, mi nombre puede ser: " + getAID().getName());
 		
+		/**
+		 *Metodo que agrega un comportamiento ciclico
+	     * El cual recibe un mensaje y dependiendo del texto, responde.
+		 */
 		addBehaviour(new CyclicBehaviour() {
 			
 			@Override
 			public void action() {
-				
+				//Se toma el mensaje de la pila de mensajes
 				ACLMessage mensaje = receive();
 				if(mensaje!=null) {
+					//Se crea un mensaje de respuesta
 					ACLMessage respuesta = mensaje.createReply();
+					//Si el mensaje contiene un Hola
 					if(mensaje.getContent().contains("Hola")) {
 						
 						respuesta.setContent("holaaaa, ¿como estas?");
@@ -28,21 +34,21 @@ public class SegundoAgente extends Agent{
 							
 						}
 					}
-					
-					JOptionPane.showMessageDialog(null, "Tu mensaje os ha sido recibido, y es: " +mensaje.getContent());
+					//Se muestra el contenido del mensaje
+					JOptionPane.showMessageDialog(null,mensaje.getSender().getLocalName() + " Tu mensaje os ha sido recibido, y es: " +mensaje.getContent());
+					//Pausa de 3 segundos
 					try{ 
 						Thread.sleep(3000); 
 					} catch(InterruptedException e ){ 
 						System.out.println("Thread Interrupted");
 						
 					}
+					//Se envia la respuesta
 					myAgent.send(respuesta);
 
 				}else block();
 				
-			
-			}
-			
+			}	
 		});
 	}
 }
