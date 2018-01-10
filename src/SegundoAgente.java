a010adea4910093dea705f1e79626b9b006d09c0
JoaquinSolanoA <33550023+JoaquinSolanoA@users.noreply.github.com>
2018-01-10 12:06:09 -0300

    Creado metodo para recibir mensajes

--- src/SegundoAgente.java
+++ src/SegundoAgente.java
@@ -1,8 +1,26 @@
+import javax.swing.JOptionPane;
+
 import jade.core.Agent;
+import jade.core.behaviours.*;
+import jade.lang.acl.*;
 
 public class SegundoAgente extends Agent{
 
 	protected void setup() {
 		System.out.println("Hey!, mi nombre puede ser: " + getAID().getName());
+		
+		addBehaviour(new CyclicBehaviour() {
+			
+			@Override
+			public void action() {
+				
+				ACLMessage msg = receive();
+				if(msg!=null) {
+					JOptionPane.showMessageDialog(null, "Tu mensaje os ha sido recibido, y es: " +msg.getContent());
+					System.out.println("¡No me gusto tu mensaje!");
+				}else block();
+			}
+			
+		});
 	}
 }
