b9141709eadeb7dab4d1cb5ed53753ae64e283c3
Baldo Morales Choque <32346079+BaldoMC@users.noreply.github.com>
2018-01-11 11:51:40 -0300

    Corregido error de "Muertes"

--- src/PrimerAgente.java
+++ src/PrimerAgente.java
@@ -1,5 +1,7 @@
 import jade.core.Agent;
-import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.*;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
 
 import javax.swing.JOptionPane;
@@ -7,56 +9,72 @@ import javax.swing.JOptionPane;
 import jade.core.AID;
 
 public class PrimerAgente extends Agent {
-	
+
+
+
 	protected void setup() {
 		System.out.println("Hola!, mi nombre es: " + getAID().getName());
-		
-		//Se crea un mensaje con PERFORMATIVE = INFORM
-		ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
-		//Se agrega el receptor a Joaquin
-		msg.addReceiver(new AID("Joaquin", AID.ISLOCALNAME));
-		//Se agrega el contenido del mensaje
-		msg.setContent("Hola");
-		//Se define el lenguaje del mensaje
-		msg.setLanguage("Espaniol");
-		//Se envia el mensaje
-		send(msg);
-		
-	    /**
-	     * Metodo que agrega un comportamiento ciclico
-	     * El cual recibe un mensaje y dependiendo del texto, responde.
-	     */
-		addBehaviour(new CyclicBehaviour() {
-		
-			@Override
-			public void action() {
-				//Se obtiene el mensaje de la pila de mensajes
-				ACLMessage mensaje = receive();
-				if(mensaje!= null) {
-					//Se crea una respuesta a ese mensaje
-					ACLMessage respuesta = mensaje.createReply();
-					if(mensaje.getContent().contains("como estas")) {
-						
-						respuesta.setContent("bien y tu?");
-					
-					}else {
-						if(mensaje.getContent().contains("chao")) {
-							
-							respuesta.setContent("Chaaaao");
-						
-						}
-					}
-					//Se muestra por un mensaje el contenido
-					JOptionPane.showMessageDialog(null,mensaje.getSender().getLocalName() +": " + mensaje.getContent());
 
-					//Se envia la respuesta
-					myAgent.send(respuesta);
-				}else
-					block();
-				
-			}
-		});
+			addBehaviour(new OneShotBehaviour() {
+
+				@Override
+				public void action() {
+					//Se crea un mensaje con PERFORMATIVE = INFORM
+					ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+					//Se agrega el receptor a Joaquin
+					msg.addReceiver(new AID("Joaquin", AID.ISLOCALNAME));
+					//Se agrega el contenido del mensaje
+					msg.setContent("Hola");
+					//Se define el lenguaje del mensaje
+					msg.setLanguage("Espaniol");
+					//Se envia el mensaje
+					send(msg);
+				}
+			});
+
+			/**
+			 * Metodo que agrega un comportamiento ciclico
+			 * El cual recibe un mensaje y dependiendo del texto, responde.
+			 */
+			addBehaviour(new CyclicBehaviour() {
+
+				@Override
+				public void action() {
+
+					//Se obtiene el mensaje de la pila de mensajes
+					ACLMessage mensaje = receive();
+
+					if(mensaje!= null) {
+						//Se crea una respuesta a ese mensaje
+						ACLMessage respuesta = mensaje.createReply();
+						respuesta.setContent("");
+						if(mensaje.getContent().contains("como estas")) {
+							//Se muestra por un mensaje el contenido
+							JOptionPane.showMessageDialog(null,mensaje.getSender().getLocalName() + ": " +mensaje.getContent());
+							respuesta.setContent("bien y tu?");
+
+						}else {
+							if(mensaje.getContent().contains("chao")) {
+								//Se muestra por un mensaje el contenido
+								JOptionPane.showMessageDialog(null,mensaje.getSender().getLocalName() + ": " +mensaje.getContent());
+								respuesta.setContent("Chaaaao");
+							}else {
+								if(!mensaje.getContent().equals("")) {
+									JOptionPane.showMessageDialog(null,mensaje.getSender().getLocalName() + ": " +mensaje.getContent());
+								}
+								doDelete();
+							}
+						}
+						
+						//Se envia la respuesta
+						myAgent.send(respuesta);
+					}else
+						block();
+				}
+			});
 	}
-	
-	
+
+	protected void takeDown() {
+		System.out.println(getAID().getLocalName() +" se despide.");
+	}
 }
--- src/SegundoAgente.java
+++ src/SegundoAgente.java
@@ -10,7 +10,7 @@ public class SegundoAgente extends Agent{
 		System.out.println("Hey!, mi nombre puede ser: " + getAID().getName());
 		
 		/**
-		 *Metodo que agrega un comportamiento ciclico
+		 * Metodo que agrega un comportamiento ciclico
 	     * El cual recibe un mensaje y dependiendo del texto, responde.
 		 */
 		addBehaviour(new CyclicBehaviour() {
@@ -22,21 +22,26 @@ public class SegundoAgente extends Agent{
 				if(mensaje!=null) {
 					//Se crea un mensaje de respuesta
 					ACLMessage respuesta = mensaje.createReply();
+					respuesta.setContent("");
 					//Si el mensaje contiene un Hola
 					if(mensaje.getContent().contains("Hola")) {
-						
+						//Se muestra por un mensaje el contenido
+						JOptionPane.showMessageDialog(null,mensaje.getSender().getLocalName() + ": " +mensaje.getContent());
 						respuesta.setContent("holaaaa, ¿como estas?");
 	
 					}else {
 						if(mensaje.getContent().contains("bien y tu")) {
-							
+							//Se muestra por un mensaje el contenido
+							JOptionPane.showMessageDialog(null,mensaje.getSender().getLocalName() + ": " +mensaje.getContent());
 							respuesta.setContent("muy bien, ahora me tengo que ir, ¡chao!");
 							
+						}else {
+							if(!mensaje.getContent().equals("")) {
+								JOptionPane.showMessageDialog(null,mensaje.getSender().getLocalName() + ": " +mensaje.getContent());
+							}
+							doDelete();
 						}
-					}
-					//Se muestra el contenido del mensaje
-					JOptionPane.showMessageDialog(null,mensaje.getSender().getLocalName() + ": " +mensaje.getContent());
-
+					}					
 					//Se envia la respuesta
 					myAgent.send(respuesta);
 
@@ -44,5 +49,8 @@ public class SegundoAgente extends Agent{
 				
 			}	
 		});
+	}
+	protected void takeDown() {
+		System.out.println(getAID().getLocalName() +" se despide.");
 	}
 }
